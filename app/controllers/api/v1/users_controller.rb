8ed6f631d5e7ef4280c73cc2548e80925235ab80
karvel-code <velgithinji@gmail.com>
2021-09-21 11:44:07 +0300

    Add create action for user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,15 @@
 class Api::V1::UsersController < ApplicationController
+    
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            render json: @user, status: :created
+        else
+            render json: @user.errors, status: :unprocessable_entity
+        end
+    end
+    
     def show
         render json: User.find(params[:id])
     end
