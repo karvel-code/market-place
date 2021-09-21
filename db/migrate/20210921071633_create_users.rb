983e54fd8230f9c2c68a187e05215884fbc34131
karvel-code <velgithinji@gmail.com>
2021-09-21 10:48:33 +0300

    Add validation for user model

--- db/migrate/20210921071633_create_users.rb
+++ db/migrate/20210921071633_create_users.rb
@@ -1,8 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :password_digest
+      t.string :email, null: false
+      t.index :email, unique: true
+      t.string :password_digest, null: false`
 
       t.timestamps
     end
