78d83714b5a711241cd25e65b6e24ab1e431ccf7
karvel-code <velgithinji@gmail.com>
2021-09-21 11:12:02 +0300

    Add bycrpt gem to gem file

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     validates :email, uniqueness: true
     validates_format_of :email, with: /@/
     validates :password_digest, prescence: true
+
+    has_secure_password
 end
