8236915d11679ca76d66fc5cddaf11be0641b542
karvel-code <velgithinji@gmail.com>
2021-09-21 12:12:57 +0300

    Add users update route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:create]
       post 'usersShow', to: 'users#show'
+      post 'usersUpdate', to: 'users#update'
     end
   end
 end
