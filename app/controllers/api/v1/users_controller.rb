434e78d8c965c22519bb9cbcdfd39307e94185a2
karvel-code <velgithinji@gmail.com>
2021-09-21 11:40:54 +0300

    Add userr params method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,4 +2,9 @@ class Api::V1::UsersController < ApplicationController
     def show
         render json: User.find(params[:id])
     end
+
+    private
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
 end
